a386f4dbcb6c55d00d2499b4265c1fa368edd5a0
Javi Vañó <javibodi@gmail.com>
2021-10-19 09:53:04 +0200

    Removing comment remaining from postgres plugin

--- sql/sqlserver/src/plugin/type-mapping.js
+++ sql/sqlserver/src/plugin/type-mapping.js
@@ -4,11 +4,6 @@ const util = require( './../util/util' )
 class TypeMapping {
   constructor() {
     this.cumulioToClickhouse = {
-      // Below is an example of different kind of type definitions.
-      // Some are never returned in this postgres plugin since the 'data_type' field
-      // returns simplified types. However others are kept as an example to match more complex types in case your database returns 'raw types'.
-      // Types such as Json, Xml are not matched in this example since they are
-      // typically not useful in analytics applications.
       numeric: [ 'money', '^smallint$', '^integer$', '^int$', '^tinyint$', '^numeric', '^bigint$', '^decimal$',
         '^double precision$', '^serial$', '^float(\\d)?$', '^real', '^float8', '^numeric\\((.+)(.+)\\)$' ],
       datetime: [ '^date$', '^time$', '^(small)?datetime(\\d)?(offset)?$', '^timestamp$', '^timestamptz$', '^interval$',
